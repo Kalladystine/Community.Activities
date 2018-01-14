de3f3562e25d0a3a2975f08bc9877274e4a7a84c
Andrzej Kniola <hagunadari@gmail.com>
2018-01-14 16:07:41 +0100

    Added designer metadata, change target framework due to https://forum.uipath.com/t/19469, rename ChangeColorsOnImage  to ChangeColorOnImage for precision and consistency

---
+++ ImageHelpers/Community.ImageHelpers.Activities/DesignerMetadata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Activities.Presentation.Metadata;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Community.ImageHelpers.Activities
+{
+    public class DesignerMetadata : IRegisterMetadata
+    {
+        public void Register()
+        {
+        }
+    }
+}
